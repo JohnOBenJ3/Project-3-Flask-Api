7fb9197955c1cee4b2488e34c2da0cb986f3e0c9
Orrin Johnson <orrinjohnson@Orrins-MacBook-Air.local>
2019-11-19 15:58:59 -0700

    basic structure is set up and a shelters database is up and running

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify, g
+import models
+from flask_login import LoginManager
+from flask_cors import CORS
+from resources.animals import animal
+from resources.shelters import shelter
+
+
+DEBUG = True #better error message
+PORT = 8000
+
+# Initialize an instance of the Flask class.
+# This starts the website!
+app = Flask(__name__) # __name__ == "__main__"
+app.secret_key = 'asldn;alisdngl;asdnl;kadsnalksdnglkasdn'
+
+
+@app.before_request
+def before_request():
+    """Connect to the database before each request."""
+    g.db = models.DATABASE
+    g.db.connect()
+
+
+@app.after_request
+def after_request(response):
+    """Close the database connection after each request."""
+    g.db.close()
+    return response
+
+
+CORS(animal, origins=['http://localhost:3000'], supports_credentials=True) # adding this line
+app.register_blueprint(animal, url_prefix='/api/v1/shelters')
+CORS(shelter, origins=['http://localhost:3000'], supports_credentials=True) # adding this line
+app.register_blueprint(shelter, url_prefix='/api/v1/animals')
+
+
+if __name__ == '__main__':
+    models.initialize()
+    app.run(debug=DEBUG, port=PORT)
---
+++ models.py
@@ -0,0 +1,58 @@
+import datetime
+from peewee import *
+from flask_login import UserMixin
+
+DATABASE = SqliteDatabase('shelters.sqlite')
+
+
+
+# DATABASE = PostgresqlDatabase(
+# 'shelter',
+# user= 'orrinjohnson',
+# password = 'password',
+# host = 'localhost'
+# )
+#sqlite is good for development because they are easy to change
+#when the app is ready you would move to postgres
+
+
+class Shelter(Model):
+    id = PrimaryKeyField(null=False)
+    address = CharField(null=True, default='')
+    city = CharField(null=True, default='')
+    name = CharField(null=True, default='')
+    shelter_id = CharField(null=True, default='')
+    phone = CharField(null=True, default='')
+    class Meta:
+        database_table = 'shelters'
+        database = DATABASE
+
+
+
+class Animal(Model):
+    id = PrimaryKeyField(null=False)
+    name = CharField()
+    shelter_id = ForeignKeyField(Shelter, related_name='shelters')
+    breed = CharField()
+    age = CharField()
+    gender = CharField()
+    photo = CharField()
+    description = CharField()
+    class Meta:
+        database_table = 'animals'
+        database = DATABASE
+
+class Admin(UserMixin, Model):
+    id = PrimaryKeyField(null=False)
+    email = CharField(unique=True)
+    password = CharField()
+    class Meta:
+        database_table = 'admin'
+        database = DATABASE
+
+
+def initialize(): #just a method. can be named anything but if initialize makes sense, keep using it
+    DATABASE.connect() #connect to db
+    DATABASE.create_tables([Shelter, Animal, Admin], safe=True) #make sure the table is created. DON'T ERASE!!!!!<-- <--
+    print("TABLES Created")
+    DATABASE.close() #after it works, close the connection so there are not threats to an open database
