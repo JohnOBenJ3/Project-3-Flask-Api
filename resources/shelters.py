61b35a359a7bba1419ac3234f9e8f7030a07e813
Orrin Johnson <orrinjohnson@Orrins-MacBook-Air.local>
2019-11-19 16:28:22 -0700

    info from a row in the shelters table returns with json in postman

--- resources/shelters.py
+++ resources/shelters.py
@@ -9,12 +9,11 @@ shelter = Blueprint('shelters', 'shelter')
 
 @shelter.route('/', methods=['GET'])
 def get_all_shelters():
-    return "Hello World"
-    # try:
-        # shelters = [model_to_dict(shelter) for shelter in models.Shelter.select()]
-        # print(shelters)
-        # return "Hello World"
-        # return jsonify(data = shelters, status = {"code": 200, "msg": "OK"})
-    # except models.DoesNotExist:
-        # return jsonify(data = {}, status = {"code": 401, "msg": "Unauthorized"})
+    # return "Hello World"
+    try:
+        shelters = [model_to_dict(shelter) for shelter in models.Shelter.select()]
+        print(shelters)
+        return jsonify(data = shelters, status = {"code": 200, "msg": "OK"})
+    except models.DoesNotExist:
+        return jsonify(data = {}, status = {"code": 401, "msg": "Unauthorized"})
         
