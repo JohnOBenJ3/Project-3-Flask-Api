21ea81ffbdef01632f836cc557e0388f8b4e563b
Orrin Johnson <orrinjohnson@Orrins-MacBook-Air.local>
2019-11-20 10:14:45 -0700

    the edit route for the animal model is working in postman

--- resources/animals.py
+++ resources/animals.py
@@ -33,3 +33,12 @@ def display_one_animal(id):
     animal = models.Animal.get_by_id(id)
     return jsonify(data = model_to_dict(animal), status = {"code": 200, "msg": "OK"})
 
+@animal.route('/<id>', methods=["PUT"])
+def update_animal(id):
+    payload = request.get_json()
+    query = models.Animal.update(**payload).where(models.Animal.id == id)
+    query.execute()
+    animal = models.Animal.get_by_id(id)
+    animal_dict = model_to_dict(animal)
+    return jsonify(data = animal_dict, status = {"code": 200, "msg": "OK"})
+
