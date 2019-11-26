c74afb333a92e010cc6cea24a9d7fc84c400365b
cnease10 <cierra.nease10@gmail.com>
2019-11-26 13:31:49 -0700

    bleh

--- seed_db.py
+++ seed_db.py
@@ -1,1 +1,32 @@
-import csv
+from models import Shelter
+created_shelter = Shelter.create(
+    name='Aurora Animal Shelter',
+    city='Denver',
+    address="3985 S. Grant St.",
+    phone="818-055-3121"
+
+)
+Shelter.create(
+    name='Doggie Days',
+    city='Denver',
+    address="3233 Middlesborough Blvd.",
+    phone="818-345-0199"
+)
+Shelter.create(
+    name='No Feline Left Behind',
+    city='Denver',
+    address="4242 Main St.",
+    phone="818-069-3125"
+)
+Shelter.create(
+    name='Animals For All',
+    city='Denver',
+    address="101 Main St.",
+    phone="818-217-8238"
+)
+Shelter.create(
+    name="Bennet's Birds",
+    city='Denver',
+    address="5965 Eagleway St.",
+    phone="867-5309"
+)
